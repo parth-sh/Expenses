5ba9f90000f9e236de01506ff7f3d9345dfbefdc
parth <parth@tradecred.com>
2022-01-03 03:59:37 +0530

    activity_main ui added

--- app/src/main/java/com/example/expenses/MainActivity.java
+++ app/src/main/java/com/example/expenses/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.expenses;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton add_items;
+    RecyclerView items_list;
+    TextView items_count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        add_items = findViewById(R.id.activity_main_fab_add_items);
+        items_list = findViewById(R.id.activity_main_rv_items_list);
+        items_count = findViewById(R.id.activity_main_tv_items_count);
     }
 }
